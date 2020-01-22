57d40554a46c0ecb8adf22d7bd7834a439cf4c56
unknown <gorisab2@gmail.com>
2020-01-22 21:47:33 +0530

    first

---
+++ nep.py
@@ -0,0 +1,35 @@
+#x = 5
+#y = 6
+#print("The multiplication of x and y is ",x*y)
+
+#x=5
+#y=9
+#print("The {2} is the multiplication of {0} and {1}".format(x,y,x*y))
+
+
+
+#light = "Red"
+#if light == "Red":
+#   print("Stop")
+#else:
+#    print("Go")
+
+#x = 10
+#if x >= 0:
+ #   print("Positive")
+#else:
+#    print("Negative")    
+    
+"""def sumf(ag1,ag2):
+    print(ag1 + ag2)
+sumf(10 , 40)"""
+
+def sum(n1,n2):
+    if type(n1) == type(n2):
+        return n1 + n2
+    else:
+         "Datatypes are different"
+         
+x=sum("G","Yes")
+print(x)
+    
---
+++ pp.py
@@ -0,0 +1,5 @@
+def ant(n1,n2):
+    c = n1+n2
+    return c
+s = ant(10,20)
+print(s)
